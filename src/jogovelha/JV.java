9fc6e5061e48ac73892bd7cf3f65b6d14025a1a2
Lívia Caroline <livia.carolline.93@gmail.com>
2015-05-27 15:28:42 -0300

    Fazendo o primeiro commit do projeto

---
+++ src/jogovelha/JV.java
@@ -0,0 +1,30 @@
+package jogovelha;
+
+import java.util.Scanner;
+
+public class JV {
+
+	      public static void main(String[] args) {
+	    	  
+	    //Criação das variaveis
+	    	  
+	        	Scanner scanner= new Scanner(System.in);
+	            int ganhou = 0, escolheJogador=0;
+	            char posicao;
+	            char tabuleiro[][]=new char [3][3];
+	        	tabuleiro[0][0]='1';
+	        	tabuleiro[0][1]='2';
+	        	tabuleiro[0][2]='3';
+	        	tabuleiro[1][0]='4';
+	        	tabuleiro[1][1]='5';
+	        	tabuleiro[1][2]='6';
+	        	tabuleiro[2][0]='7';
+	        	tabuleiro[2][1]='8';
+	        	tabuleiro[2][2]='9';
+
+
+
+
+}
+	      
+}
